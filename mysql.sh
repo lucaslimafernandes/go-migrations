107ec1b4835b00f106e57480dd25254764727063
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-09-28 19:47:47 -0300

    ADD Migrations to configs.yaml

--- mysql.sh
+++ mysql.sh
@@ -3,7 +3,7 @@ sudo docker run --rm -d \
     -e MYSQL_ROOT_PASSWORD=root_password \
     -e MYSQL_DATABASE=mydb \
     -e MYSQL_USER=myuser \
-    -e MYSQL_PASSWORD=user_password \
+    -e MYSQL_PASSWORD=password \
     -p 3306:3306 \
     -d mysql:9.0.1
 
